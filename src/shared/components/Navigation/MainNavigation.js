9b65cfb1150910868ae1b3885de5dbd8168e5466
utkarsh032 <utkarshhraj525@gmail.com>
2023-06-07 14:49:02 +0530

    Addding NavLinks

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './MainNavigation.css'
 import { Link } from 'react-router-dom'
 import MainHeader from './MainHeader'
+import NavLinks from './NavLinks'
 
 const MainNavigation = props => {
   return (
@@ -14,7 +15,9 @@ const MainNavigation = props => {
       <h1 className='main-navigation__title'>
         <Link to='/'>Your Places</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   )
 }
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-const NavLinks = () => {
+import './NavLinks.css'
+
+const NavLinks = props => {
   return (
-    <div>NavLinks</div>
+    <ul className='nav-links'>
+      <li>
+        <NavLink to='/' exact>
+          ALL USERS
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/u1/places'>MYPLACES</NavLink>
+      </li>
+      <li>
+        <NavLink to='/places/new'>ADD PLACE</NavLink>
+      </li>
+      <li>
+        <NavLink to='/auth'>AUTHENTICATE</NavLink>
+      </li>
+    </ul>
   )
 }
 
