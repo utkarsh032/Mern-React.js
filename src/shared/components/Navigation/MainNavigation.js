8d2ea6dba1268fefd12b88e844110b7b1a298121
utkarsh032 <utkarshhraj525@gmail.com>
2023-06-08 10:04:13 +0530

    Drawer, sidedrawer animation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import MainHeader from './MainHeader'
@@ -6,16 +6,34 @@ import NavLinks from './NavLinks'
 import SideDrawer from './SideDrawer'
 import './MainNavigation.css'
 
+import Backdrop from '../UIElements/Backdrop'
+
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true)
+  }
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false)
+  }
+
   return (
     <React.Fragment>
-      <SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className='main-navigation__drawer-nav'>
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className='main-navigation__menu-btn'>
+        <button
+          className='main-navigation__menu-btn'
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
