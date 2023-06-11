bd2139c654adc051877255f6f6daef9683d6b15f
utkarsh032 <utkarshhraj525@gmail.com>
2023-06-11 23:46:01 +0530

    Adding form Input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import './NewPlace.css'
 
+import Input from '../../shared/components/FormElements/Input'
+
 const NewPlace = () => {
-  return <h2>NewPlace is under the process</h2>
+  return (
+    <form className='place-form'>
+      <Input
+        element='input'
+        type='text'
+        label='Title'
+        validators={[]}
+        errorText='Please enter a valid title'
+      />
+    </form>
+  )
 }
 
 export default NewPlace
