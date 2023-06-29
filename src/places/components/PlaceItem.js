c144eb2d9e4b1d3faa878869c8fada434c05f048
utkarsh032 <utkarshhraj525@gmail.com>
2023-06-29 08:41:51 +0530

    Showing Deletion Warning

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -8,11 +8,25 @@ import './PlaceItem.css'
 
 const PlaceItem = props => {
   const [showMap, setShowMap] = useState(false)
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
 
   const openMapHandler = () => setShowMap(true)
 
   const closeMapHandler = () => setShowMap(false)
 
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true)
+  }
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false)
+    console.log('Canceling...')
+  }
+
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false)
+    console.log('DELETING...')
+  }
   return (
     <React.Fragment>
       <Modal
@@ -27,6 +41,26 @@ const PlaceItem = props => {
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
+
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header='Are you Sure?'
+        footerClass='place-item__modal-actions'
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              Cancel
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              Delete
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>Do you eant to proceed or delete this place ?</p>
+      </Modal>
+
       <li className='place-item'>
         <Card className='place-item__content'>
           <div className='place-item__image'>
@@ -42,7 +76,9 @@ const PlaceItem = props => {
               VIEW ON MAP
             </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={showDeleteWarningHandler}>
+              DELETE
+            </Button>
           </div>
         </Card>
       </li>
