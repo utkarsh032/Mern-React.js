d7d5bc19dfc4083704e0f86f7c00df5ef3b6ccf2
utkarsh032 <utkarshhraj525@gmail.com>
2023-06-29 07:36:29 +0530

    Adding Form-Hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,95 +1,126 @@
-import React from 'react'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
-import './PlaceForm.css'
-
 import {
-  VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
 } from '../../shared/util/validators'
+import { useForm } from '../../shared/hooks/form-hook'
+import './PlaceForm.css'
 
 const DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
-    description: 'One of the most famour sky Scraper in the world!',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      'https://cdn.getyourguide.com/img/location/5ca347fb624aa.jpeg/99.webp',
-    address: '20 W 34th St., New York, NY 10001, United States',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
-      lat: 40.7484404,
-      lng: 73.9905353
+      lat: 40.7484405,
+      lng: -73.9878584
     },
     creator: 'u1'
   },
   {
     id: 'p2',
     title: 'Empire State Building',
-    description: 'One of the most famour sky Scraper in the world!',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      'https://lh3.googleusercontent.com/p/AF1QipNVlM5lo7fIJrmvjN4EOrTMiQjDgDyTfw7ATdV6=s1360-w1360-h1020',
-    address: '20 W 34th St., New York, NY 10001, United States',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
-      lat: 40.7484404,
-      lng: 73.9905353
+      lat: 40.7484405,
+      lng: -73.9878584
     },
     creator: 'u2'
-  },
-
-  {
-    id: 'p3',
-    title: 'Empire State Building',
-    description: 'One of the most famour sky Scraper in the world!',
-    imageUrl: 'https://cdn.getyourguide.com/img/tour/64288fe87a85d.jpeg/98.jpg',
-    address: '20 W 34th St., New York, NY 10001, United States',
-    location: {
-      lat: 40.7484404,
-      lng: 73.9905353
-    },
-    creator: 'u3'
   }
 ]
 
 const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const placeId = useParams().placeId
 
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+    false
+  )
+
   const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId)
+
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true
+        }
+      },
+      true
+    )
+    setIsLoading(false)
+  }, [setFormData, identifiedPlace])
+
+  const placeUpdateSubmitHandler = event => {
+    event.preventDefault()
+    console.log(formState.inputs)
+  }
 
   if (!identifiedPlace) {
     return (
       <div className='center'>
-        <h2>Could not find Place!</h2>
+        <h2>Could not find place!</h2>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div className='center'>
+        <h2>Loading...</h2>
       </div>
     )
   }
 
   return (
-    <form className='place-form'>
+    <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
       <Input
         id='title'
         element='input'
         type='text'
         label='Title'
         validators={[VALIDATOR_REQUIRE()]}
-        errorText='Please enter valid title'
-        onInput={() => {}}
-        value={identifiedPlace.title}
-        valid={true}
+        errorText='Please enter a valid title.'
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
       />
-
       <Input
         id='description'
         element='textarea'
-        label='Descrition'
+        label='Description'
         validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText='Please enter valid desccription (min. 5 characters).'
-        onInput={() => {}}
-        value={identifiedPlace.description}
-        valid={true}
+        errorText='Please enter a valid description (min. 5 characters).'
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
       />
-
-      <Button type='submit' disabled={false}>
+      <Button type='submit' disabled={!formState.isValid}>
         UPDATE PLACE
       </Button>
     </form>
